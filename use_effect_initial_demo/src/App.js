67d81e3e7c0079cfb4aab29675000582764609cf
mrarthurwhite <mister.arthur.white@gmail.com>
2021-06-27 18:58:40 -0400

    added backend data using json server

--- use_effect_initial_demo/src/App.js
+++ use_effect_initial_demo/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {  useEffect, useState } from 'react';
+
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [words, setWords] = useState([]);
+
+  let fetchData = () => {
+                          fetch('http://localhost:1111/words')
+                          .then(response => response.json())
+                          .then (data =>
+                                            {
+                                                  setWords(data);
+                                            } 
+                                          )
+                        };
+
+  useEffect(fetchData, [] );
+
+
+    return (
+    <>
+    {
+      words.map(w=> <div>{w.word}</div>)
+    }
+    </>
   );
 }
 
 export default App;
+/**
+ *
+ * In functional components, you use the useEffect Hook to fetch data when the component loads or some information changes.  You’ll also need to save the results with the useState Hook.   
+ */
