5daf206e7b17cce53d618eab7f96d30c913ab6fd
mrarthurwhite <mister.arthur.white@gmail.com>
2021-07-03 14:49:18 -0400

    edited some of the react jsx to fix error

--- use_effect_initial_demo/src/index.js
+++ use_effect_initial_demo/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import GetWordsWAxios from './GetWordsWAxios';
+//import App from './App';
+//import GetWordsWAxios from './GetWordsWAxios';
 import GetWordsWAxiosNLoading from './GetWordsWAxiosNLoading';
 //    <App /> <GetWordsWAxios />
 
